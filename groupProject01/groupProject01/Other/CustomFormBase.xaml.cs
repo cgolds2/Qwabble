370bd5244844ff0b8752d076496b36b5ea758db0
Connor Goldsmith <cgolds2@lsu.edu>
2017-03-13 22:33:06 -0500

    Made custom form base for caroline

---
+++ groupProject01/groupProject01/Other/CustomFormBase.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace groupProject01.Other
+{
+	public partial class CustomFormBase : ContentPage
+	{
+		public CustomFormBase ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
