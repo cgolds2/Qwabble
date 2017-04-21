b3ecd94d1e0cc04cd4aff30d43e02f6abcec14d0
ssicard <ssicar1@lsu.edu>
2017-04-20 21:55:56 -0500

    Created navigation for initial login pages, added methods for server tester

---
+++ groupProject01/groupProject01/PrevApartmentPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace groupProject01
+{
+	public partial class PrevApartmentPage : ContentPage
+	{
+        GlobalData _gd;
+		public PrevApartmentPage (GlobalData gd)
+		{
+            _gd = gd;
+			InitializeComponent ();
+		}
+
+        async void OnSubmit(object sender, EventArgs e)
+        {
+            //check for apartment
+            await Navigation.PushAsync(new groupProject01.MainPage(_gd));
+            //await Navigation.PushAsync(new groupProject01.HomePage(_gd));
+
+
+        }
+    }
+}
