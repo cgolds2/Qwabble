bc37bfad76d47e0db0a6b8276194181226d7d419
ssicard <ssicar1@lsu.edu>
2017-04-20 22:06:34 -0500

    Set up better navigation between login pages, added settings page set up

--- groupProject01/groupProject01/Settings/SettingsPage.xaml.cs
+++ groupProject01/groupProject01/Settings/SettingsPage.xaml.cs
@@ -12,57 +12,17 @@ namespace groupProject01
 	{
 		public SettingsPage ()
 		{
-			            NavigationPage.SetHasNavigationBar(this, false); InitializeComponent();
+			InitializeComponent ();
 		}
-        #region BottomBarStuff
 
-        public void OnHome(object sender, EventArgs e)
+        void OnLogout(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
-        }
-        public void OnList(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnCalendar(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnMessaging(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnSetting(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
+            //TODO logout user
         }
 
-        #endregion
-        public void onPushNotificationClicked(object Sender, EventArgs e)
+        void OnCurApartment(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
-        }
-
-        public void onThemeClicked(object Sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void onLogOutClicked(object Sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void onChangeAptClicked(object Sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Redirects to current apartment selection.
-        /// </summary>
-        /// <param name="Sender"></param>
-        /// <param name="e"></param>
-        public void onCurrentAptClicked(object Sender, EventArgs e)
-        {
-            throw new NotImplementedException();
+            //TODO show current apartment
         }
     }
 }
