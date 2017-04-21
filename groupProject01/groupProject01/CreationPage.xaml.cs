fe76bbf31cb4a739c00b77415902eea34dd6371e
ssicard <ssicar1@lsu.edu>
2017-04-20 21:12:42 -0500

    Added introductory pages for initial and create apartment

---
+++ groupProject01/groupProject01/CreationPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace groupProject01
+{
+	public partial class CreationPage : ContentPage
+	{
+		public CreationPage (GlobalData gd)
+		{
+			InitializeComponent ();
+		}
+
+        async void OnNewApartment(object sender, EventArgs e)
+        {
+
+
+        }
+
+        async void OnPrevApartment(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ groupProject01/groupProject01/InitialPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace groupProject01
+{
+	public partial class InitialPage : ContentPage
+	{
+        GlobalData _gd;
+
+        public InitialPage (GlobalData gd)
+		{
+			InitializeComponent ();
+		}
+
+        async void OnLogin(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new groupProject01.LoginPage(_gd));
+
+        }
+        async void OnCreateUser(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new groupProject01.CreationPage(_gd));
+
+        }
+    }
+}
