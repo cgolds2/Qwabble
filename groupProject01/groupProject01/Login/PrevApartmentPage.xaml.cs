dc3c9ab560fcac2adae97f0ea51eb0f37940279f
ssicard <ssicar1@lsu.edu>
2017-04-21 20:37:10 -0500

    added await to fix a warning

--- groupProject01/groupProject01/Login/PrevApartmentPage.xaml.cs
+++ groupProject01/groupProject01/Login/PrevApartmentPage.xaml.cs
@@ -24,11 +24,11 @@ namespace groupProject01
         async void OnSubmit(object sender, EventArgs e)                 //when submit button is pressed
         {
             int apartmentID = int.Parse(apartment.Text);
-            List<ApartmentObject> aptLs = await(ServerHandeler.getApartments(_gd.CurrentUser.UserID, apartmentID)); //gets the apartment with the associated userid
+            List<ApartmentObject> aptLs = await(ServerHandeler.getApartments()); //gets the apartment with the associated userid
             ApartmentObject aptObj = findApartment(aptLs, apartmentID);
             if(aptObj != null)                                                      //if apartmentid is not null
             {
-                ServerHandeler.addUserToApartment(_gd.CurrentUser.UserID, apartmentID);
+                await ServerHandeler.addUserToApartment(_gd.CurrentUser.UserID, apartmentID);
                 await Navigation.PushAsync(new groupProject01.HomePage(_gd));       //direct to home page
             }
             else                                                                    //if apartmentid is null
