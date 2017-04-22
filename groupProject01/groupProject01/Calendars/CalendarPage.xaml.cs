ebff3bc66f38f80a0b960a18e1013a419e4f6935
ssicard <ssicar1@lsu.edu>
2017-04-21 21:18:48 -0500

    Made all the buttons at the buttom work

--- groupProject01/groupProject01/Calendars/CalendarPage.xaml.cs
+++ groupProject01/groupProject01/Calendars/CalendarPage.xaml.cs
@@ -18,30 +18,6 @@ namespace groupProject01
         {
             InitializeComponent();
         }
-        #region BottomBarStuff
-
-        public void OnHome(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnList(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnCalendar(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnMessaging(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        public void OnSetting(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
        
             public ObservableCollection<CalendarOptionObject> calendar { get; set; } //declares calendars ObservableCollection that will notify when calendars are added, removed, and refreshed
             private GlobalData _gd;                                              //declares global variable GlobalData to store the global data
@@ -54,20 +30,31 @@ namespace groupProject01
                 calView.ItemsSource = calendar;                                  //sets the source of items to template and display
             }
 
-            async void OnTap(object sender, ItemTappedEventArgs e)                    //Lister that fires when a calendar item is tapped
+        #region BottomBarButtons
+
+        async public void OnHome(object sender, EventArgs e)                          //if home button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.HomePage(_gd));             //direct to home page
+        }
+        async public void OnMessaging(object sender, EventArgs e)                      //if messaging button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.MessagingPage(_gd));         //direct to messaging page
+        }
+        async public void OnList(object sender, EventArgs e)                          //if lists button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.ListsPage(_gd));            //if lists button is pressed
+        }
+        async public void OnSetting(object sender, EventArgs e)                       //if setting button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.SettingsPage(_gd));            //if setting button is pressed
+        }
+
+        #endregion
+
+        async void OnTap(object sender, ItemTappedEventArgs e)                    //Lister that fires when a calendar item is tapped
             {
                 int index = ((CalendarOptionObject)e.Item).ID;                           //gets the ID of the calendar item
                 await Navigation.PushAsync(new groupProject01.EventPage(index, _gd)); //opens the event page as current page continues to run
             }
-
-
-
-            /* METHODS TO ADD:
-             * createNewEvent
-             * scheduleNewGuest(s)
-             * scheduleChore
-             * reserveLaundryMachines
-             * reserveQuiettime
-             */
         }
     }
--- groupProject01/groupProject01/Messaging/MessagingPage.xaml.cs
+++ groupProject01/groupProject01/Messaging/MessagingPage.xaml.cs
@@ -22,6 +22,27 @@ namespace groupProject01
             msgView.ItemsSource = groups;                    //sets the messaging UI
 		}
 
+        #region BottomBarButtons
+
+        async public void OnHome(object sender, EventArgs e)                          //if home button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.HomePage(_gd));             //direct to home page
+        }
+        async public void OnCalendar(object sender, EventArgs e)                      //if calendar button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.CalendarPage(_gd));         //direct to calendar page
+        }
+        async public void OnList(object sender, EventArgs e)                          //if lists button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.ListsPage(_gd));            //if lists button is pressed
+        }
+        async public void OnSetting(object sender, EventArgs e)                       //if setting button is pressed
+        {
+            await Navigation.PushAsync(new groupProject01.SettingsPage(_gd));            //if setting button is pressed
+        }
+
+        #endregion
+
         async void OnTap(object sender, ItemTappedEventArgs e)      //a listener that fires when a message is tapped
         {
             //int index = ((MessageObject)e.Item).ID;    //gets the ID of the messaging item (sender=row)
