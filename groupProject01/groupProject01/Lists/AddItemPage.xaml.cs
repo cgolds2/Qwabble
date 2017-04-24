056fae3a271e7635003a682bf72d662e33fa1dff
ssicard <ssicar1@lsu.edu>
2017-04-24 11:35:34 -0500

    implemented addItem method

--- groupProject01/groupProject01/Lists/AddItemPage.xaml.cs
+++ groupProject01/groupProject01/Lists/AddItemPage.xaml.cs
@@ -12,9 +12,29 @@ namespace groupProject01
 	
 	public partial class AddItemPage : ContentPage
 	{
-		public AddItemPage ()
+        GlobalData _gd;
+		public AddItemPage (GlobalData gd)
 		{
+            gd = _gd;
 			InitializeComponent ();
 		}
-	}
+        
+        async void OnAddItem(object sender, EventArgs e)
+        {
+            ListItemObject it = new ListItemObject();
+            it.noteName = itemNameTextField.Text;
+            //set it.ListID
+            if (itemDetailTextField.Text.Equals(""))
+            {
+                it.listType = 0;
+            }
+            else
+            {
+                it.listType = 1;
+            }
+            it.data = itemDetailTextField.Text;
+            ServerHandeler.sendList(it, _gd);
+            await Navigation.PushAsync(new groupProject01.ListsPage(_gd));
+        }
+    }
 }
