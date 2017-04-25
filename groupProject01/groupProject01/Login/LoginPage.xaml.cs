c08d8cf3f19a59b52008887db9284f3f235c3ad5
ssicard <ssicar1@lsu.edu>
2017-04-25 18:29:23 -0500

    worked on implementing logout

--- groupProject01/groupProject01/Login/LoginPage.xaml.cs
+++ groupProject01/groupProject01/Login/LoginPage.xaml.cs
@@ -14,17 +14,18 @@ namespace groupProject01
 
 		public LoginPage (GlobalData gd)
 		{
-			            NavigationPage.SetHasNavigationBar(this, false); InitializeComponent();
-            _gd = gd;
+			NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();      //draws the UI components of the page
+            _gd = gd;                   //sets local instance of global data
 		}
 
-        async void OnSubmit(object sender, EventArgs e)
+        async void OnSubmit(object sender, EventArgs e)     //when the submit button is clicked
         {
             //TODO something to check username and password
-            await Navigation.PushAsync(new groupProject01.MainPage(_gd),false);
-            //await Navigation.PushAsync(new groupProject01.HomePage(_gd),false);
-
-
+            //if valid
+            await Navigation.PushAsync(new groupProject01.HomePage(_gd),false);     //sends the user to home page
+            //if not valid
+            //DisplayAlert("Wrong Username/Password", "Your username or password is incorrect. Reenter.", "Ok");
         }
     }
 }
