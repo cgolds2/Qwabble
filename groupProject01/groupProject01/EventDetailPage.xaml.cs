41cbc489e3ed8a73575922550f93e3c4812863a0
ssicard <ssicar1@lsu.edu>
2017-04-26 22:59:14 -0500

    created event detail page

---
+++ groupProject01/groupProject01/EventDetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace groupProject01
+{
+	public partial class EventDetailPage : ContentPage
+	{
+		public EventDetailPage (EventObject currentEvent, GlobalData gd)
+		{
+            NavigationPage.SetHasNavigationBar(this, false);
+			InitializeComponent ();
+
+            lblLabel.Text = currentEvent.name;
+		}
+	}
+}
