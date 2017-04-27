4d4b9217274fb3d30fd8a24996ca833f5eb98f70
ssicard <ssicar1@lsu.edu>
2017-04-27 11:27:13 -0500

    worked to fix add button on events

--- groupProject01/groupProject01/Calendars/AddEventPage.xaml.cs
+++ groupProject01/groupProject01/Calendars/AddEventPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace groupProject01
             NavigationPage.SetHasNavigationBar(this, false);                                        //hides the default naviagion bar
 
             _gd = gd;
-			InitializeComponent ();
+            InitializeComponent();
 		}
 
         async void OnAddEvent(object sender, EventArgs e)
